1848450107423d6ff142297b58028f0300015562
pirthika <131761932+pirthika@users.noreply.github.com>
2023-05-17 21:03:32 +0530

    Add files via upload

---
+++ looping programs/loop7.py
@@ -0,0 +1,28 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/Users/pooja_M/Desktop/loop7.py ==================
+celcius	Fahrenheit
+32.0 	 0
+50.0 	 10
+68.0 	 20
+86.0 	 30
+104.0 	 40
+122.0 	 50
+140.0 	 60
+158.0 	 70
+176.0 	 80
+194.0 	 90
+>>> 
+================== RESTART: C:/Users/pooja_M/Desktop/loop7.py ==================
+celcius	Fahrenheit
+0 	 32.0
+10 	 50.0
+20 	 68.0
+30 	 86.0
+40 	 104.0
+50 	 122.0
+60 	 140.0
+70 	 158.0
+80 	 176.0
+90 	 194.0
