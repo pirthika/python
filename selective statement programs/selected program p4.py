ebc32009a4c2f4e9c683ee7afc800075289d90f3
pirthika <131761932+pirthika@users.noreply.github.com>
2023-05-17 20:53:33 +0530

    Add files via upload

---
+++ selective statement programs/selected program p4.py
@@ -0,0 +1,8 @@
+date=int(input("Enter the date:"))
+month=int(input("Enter the month:"))
+year=int(input("Enter the year:"))
+age=2023-year
+if(age>60):
+    print("The fare will be",(1020-1020*(20/100)))
+else:
+    print("The fare will be 1020")
