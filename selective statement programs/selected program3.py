cebf5be2359244c11ebee8e92f9cb87d7d51f0af
pirthika <131761932+pirthika@users.noreply.github.com>
2023-05-17 20:51:59 +0530

    Add files via upload

---
+++ selective statement programs/selected program3.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the number:"))
+p=int(input("Enter the power:"))
+c=n**p
+if(c%2==0):
+    print("The nth power of a no is even")
+else:
+    print("The nth power of a no is odd")
