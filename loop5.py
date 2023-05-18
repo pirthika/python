f66b924031bdb29cd0ecdf5cbb164461ccd9ac66
pirthika <131761932+pirthika@users.noreply.github.com>
2023-05-18 11:25:15 +0530

    Delete loop5.py

--- loop5.py
+++
@@ -1,7 +0,0 @@
-'''Write a program to display the multiplication table as below with a given range
-Sample Input
-Start Table : 5
-End Table: 8'''
-for i in range(5,9):
-   for j in range(1,11):
-    print(j,"*",i,"=",j*i)
